d01db107f01e939df87d6e0534d00a344795524a
rock051 <fernando.roque@estudante.ufla.br>
2023-07-15 23:07:30 -0300

    correções ainda existe 3 problemas

--- trabalho/lib/main.dart
+++ trabalho/lib/main.dart
@@ -28,8 +28,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    String disciplina = '', codigo = '', nomeProf = '';
-    String prova1 = '', prova2 = '', trabalho = '';
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
